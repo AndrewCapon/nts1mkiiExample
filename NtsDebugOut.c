4a98c581da6499006a4c5d6ad3d2d3d37e3ac2f1
AndrewCapon <andrew.capon@zen.co.uk>
2024-05-29 13:06:05 +0100

    Remove unneeded includs

--- NtsDebugOut.c
+++ NtsDebugOut.c
@@ -9,14 +9,7 @@
 #include "core_cm7.h"
 
 #include "stm32h7xx_ll_rcc.h"
-#include "stm32h7xx_ll_crs.h"
 #include "stm32h7xx_ll_bus.h"
-#include "stm32h7xx_ll_system.h"
-#include "stm32h7xx_ll_exti.h"
-#include "stm32h7xx_ll_cortex.h"
-#include "stm32h7xx_ll_utils.h"
-#include "stm32h7xx_ll_pwr.h"
-#include "stm32h7xx_ll_dma.h"
 #include "stm32h7xx_ll_usart.h"
 #include "stm32h7xx_ll_gpio.h"
 
